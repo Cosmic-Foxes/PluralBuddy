7a9dd218e5870395d8fd2e5b638810ac810a1195
giftedl <52332868+giftedl@users.noreply.github.com>
2026-06-10 21:00:56 -0500

    Bump plurography version & control if null to avoid NaN

--- packages/bot/src/lib/get-system-flags.ts
+++ packages/bot/src/lib/get-system-flags.ts
@@ -2,24 +2,24 @@ import { SystemFlags, type PSystem } from "plurography";
 
 export function getSystemFeatures(data: PSystem) {
 	return {
-		keepProxyTags: (data.flags & SystemFlags.KEEP_PROXY_TAGS) !== 0,
+		keepProxyTags: ((data.flags ?? 0) & SystemFlags.KEEP_PROXY_TAGS) !== 0,
 
-		has: (flag: SystemFlags) => (data.flags & flag) !== 0,
+		has: (flag: SystemFlags) => ((data.flags ?? 0) & flag) !== 0,
 		disable: (flag: SystemFlags) =>
-			(data.flags & flag) === 0 /* doesn't have flag */
-				? data.flags
-				: data.flags - flag,
+			(((data.flags) ?? 0) & flag) === 0 /* doesn't have flag */
+				? (data.flags ?? 0)
+				: (data.flags ?? 0) - flag,
 		enable: (flag: SystemFlags) =>
-			(data.flags & flag) !== 0 /* does have flag */
-				? data.flags
-				: data.flags + flag,
+			(((data.flags) ?? 0) & flag) !== 0 /* does have flag */
+				? (data.flags ?? 0)
+				: (data.flags ?? 0) + flag,
 		bool: (flag: SystemFlags, bool?: boolean) =>
 			bool
-				? (data.flags & flag) !== 0 /* does have flag */
-					? data.flags
-					: data.flags + flag
-				: (data.flags & flag) === 0 /* doesn't have flag */
-					? data.flags
-					: data.flags - flag,
+				? ((data.flags ?? 0) & flag) !== 0 /* does have flag */
+					? (data.flags ?? 0)
+					: (data.flags ?? 0) + flag
+				: ((data.flags ?? 0) & flag) === 0 /* doesn't have flag */
+					? (data.flags ?? 0)
+					: (data.flags ?? 0) - flag,
 	};
 }
