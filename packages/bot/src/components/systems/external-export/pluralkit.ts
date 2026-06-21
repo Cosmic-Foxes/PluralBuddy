9096d16552a77f2a6a42e16323164de8df7b6c0b
giftedl <52332868+giftedl@users.noreply.github.com>
2026-06-20 22:25:34 -0500

    Add select menu / Interaction ID for PK external exporting

---
+++ packages/bot/src/components/systems/external-export/pluralkit.ts
@@ -0,0 +1,13 @@
+import { InteractionIdentifier } from "@/lib/interaction-ids";
+import { ComponentCommand, type ComponentContext } from "seyfert";
+
+export default class PluralKitExternalExporting extends ComponentCommand {
+    componentType = 'StringSelect' as const;
+
+    override filter(ctx: ComponentContext<typeof this.componentType>) {
+        return InteractionIdentifier.Systems.Configuration.GeneralTab.SetPrivacy.equals(ctx.customId)  
+    }
+
+    async run(ctx: ComponentContext<typeof this.componentType>) {
+    }
+}
