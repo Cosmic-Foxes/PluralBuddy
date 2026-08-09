0a0d6acaf09bab6e564e20cb4eb8c707660eb9de
giftedl <52332868+giftedl@users.noreply.github.com>
2026-08-09 15:45:12 +0700

    Use correct amount for attachments being too large

--- packages/bot/src/i18n/en.ts
+++ packages/bot/src/i18n/en.ts
@@ -82,7 +82,7 @@ SimplyPlural has been discontinued.`,
 	PLURALBUDDY_IMPORT_ERROR_TOO_LARGE:
 		"2j – Importing files cannot be larger than 2MB. Please contact support if you are genuinely trying to import a system larger than 2MB.",
 	ERROR_ATTACHMENT_TOO_LARGE:
-		"2j – Attachments for banners or profile pictures cannot be larger than 2MB.",
+		"2j – Attachments for banners or profile pictures cannot be larger than 1MB.",
 	CREATE_NEW_ALTER_DESCRIPTION: "Create New Alter",
 	CREATE_NEW_TAG_DESCRIPTION: "Create New Tag",
 	PLURALBUDDY_IMPORT_ERROR_INVALID_JSON:
