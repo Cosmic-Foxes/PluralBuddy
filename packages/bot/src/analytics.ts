88f419c7b56e2c0cea76e4ac29c37a67fc52ac8d
giftedl <52332868+giftedl@users.noreply.github.com>
2026-09-13 23:43:42 -0500

    Set latest to the correct count aswell

--- packages/bot/src/analytics.ts
+++ packages/bot/src/analytics.ts
@@ -47,7 +47,7 @@ export async function gatherStatisticalData(): Promise<PAnalytics> {
 	}
 
 	await client.cache.statistic.set(CacheFrom.Gateway, "latest", {
-		guildCount: guilds.length,
+		guildCount: (await client.cache.guilds?.count()) ?? 3,
 		userCount,
 	});
 
