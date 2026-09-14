39a43f8a2b2eb471c9fb40e19ff4d5dcb4defa18
giftedl <52332868+giftedl@users.noreply.github.com>
2026-09-13 23:40:00 -0500

    Adjust for tests (again)

--- packages/bot/src/events/ready.ts
+++ packages/bot/src/events/ready.ts
@@ -6,9 +6,7 @@ import { analyticsCollection } from "@/mongodb";
 export default createEvent({
 	data: { name: "ready", once: false },
 	run: async (user, client) => {
-		if (analyticsCollection !== undefined)
-        await initializeApplicationCommands();
-		startStatisticalTimer();
-    }
-
+		await initializeApplicationCommands();
+		if (analyticsCollection !== undefined) startStatisticalTimer();
+	},
 });
