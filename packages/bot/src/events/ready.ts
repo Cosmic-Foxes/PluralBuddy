59fd3af5027ad9623f81662d56e8b29e5115a69f
giftedly <52332868+giftedl@users.noreply.github.com>
2026-09-13 23:26:21 -0500

    Format correctly

--- packages/bot/src/events/ready.ts
+++ packages/bot/src/events/ready.ts
@@ -8,7 +8,8 @@ export default createEvent({
 
 		
 		setInterval(() => {
-		client.logger.info(await client.cache.guilds?.count());}, 5000)
+		  client.logger.info(await client.cache.guilds?.count())
+		}, 5000)
     }
 
 });
