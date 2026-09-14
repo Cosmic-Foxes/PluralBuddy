e9265cceaf4b89b4d9ac6031451cce36af136d0e
giftedl <52332868+giftedl@users.noreply.github.com>
2026-09-13 23:37:52 -0500

    Adjust for tests

--- packages/bot/src/events/ready.ts
+++ packages/bot/src/events/ready.ts
@@ -1,10 +1,12 @@
 import { createEvent } from "seyfert";
 import { startStatisticalTimer } from "@/analytics";
 import { initializeApplicationCommands } from "@/lib/mention-command";
+import { analyticsCollection } from "@/mongodb";
 
 export default createEvent({
 	data: { name: "ready", once: false },
 	run: async (user, client) => {
+		if (analyticsCollection !== undefined)
         await initializeApplicationCommands();
 		startStatisticalTimer();
     }
