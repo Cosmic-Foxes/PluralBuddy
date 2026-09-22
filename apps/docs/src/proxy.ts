21b0122f600a8ed3f9ef43bb11d0392c49332956
giftedl <52332868+giftedl@users.noreply.github.com>
2026-09-22 17:48:31 -0500

    Include search params in redirect link

--- apps/docs/src/proxy.ts
+++ apps/docs/src/proxy.ts
@@ -25,7 +25,7 @@ export default function proxy(request: NextRequest, event: NextFetchEvent) {
 	if (request.nextUrl.origin === ("https://pb.giftedly.dev") && !new URL(request.url).pathname.startsWith("/api/")) {
 
 		return NextResponse.redirect(
-			new URL(`${process.env.BETTER_AUTH_URL}/${new URL(request.url).pathname}`),
+			new URL(`${process.env.BETTER_AUTH_URL}/${new URL(request.url).pathname + new URL(request.url).search}`),
 			308
 		);
 	}
